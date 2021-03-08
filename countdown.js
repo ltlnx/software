d2975e65e0379e8c7a5e1c649c082038aef9f395
ltlnx <litelinux.org@gmail.com>
2021-03-08 20:46:53 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,4 +1,4 @@
-const deadline = 'July 1 2021 00:00:00 GMT+0800';
+const deadline = 'July 3 2021 00:00:00 GMT+0800';
 
 function getTimeRemaining(endtime){
   const total = Date.parse(endtime) - Date.parse(new Date());
